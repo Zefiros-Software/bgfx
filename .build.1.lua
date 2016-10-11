afe29f3b09aab6a5842c1bd1c0fa4e7bafb8ee01
koen visscher <koen.visscher@hotmail.com>
2016-10-12 00:24:08 +0200

    Added basic settings

--- .build.1.lua
+++ .build.1.lua
@@ -1,42 +1,131 @@
+
 project "bgfx"
 
     kind "StaticLib"
  
 
-    includedirs {
-       "include",
-       "src"
-    }
-
-    -- generic
     files {
-       "include/SFML/System/*.inl",
-       "src/SFML/System/*.cpp"
-    }
+       "include/**.h",
+	"src/**.cpp",
+        "src/**.h"
+      }
 
-    filter "system:windows"
+    removefiles {
+         "src/**.bin.h",
+	}
 
+    includedirs {
+	"3rdparty",
+	"3rdparty/dxsdk/include",
+ 	"include",
+	}
+
+    filter { "system:windows", "not action:android" }
+	
+	links {
+	    "gdi32",
+	    "psapi",
+	      }
 
     filter "system:linux"
+	
+	links {
+		"X11",
+		"GL",
+		"pthread",
+	      }
 
+	buildoptions {
+		"-fPIC",
+		}
+	
+    filter { "system:macosx", "not action:ios" }
 
-    filter "system:macosx"
-
+	linkoptions {
+			"-framework Cocoa",
+			"-framework QuartzCore",
+			"-framework OpenGL",
+			"-weak_framework Metal",
+		    }   
     
+	files {
+		"src/glcontext_**.mm",
+		"src/renderer_**.mm",
+	      }
+
     filter "action:android"
 
+	links {
+		"EGL",
+		"GLESv2",
+	      }
+    filter "action:ios"
+
+	linkoptions {
+			"-framework CoreFoundation",
+			"-framework Foundation",
+			"-framework OpenGLES",
+			"-framework UIKit",
+			"-framework QuartzCore",
+			"-weak_framework Metal",
+		}
 
     filter {} 
 
     zpm.export(function()
 
-        filter "system:macosx"
+	includedirs {
+           "3rdparty",
+           "3rdparty/dxsdk/include",
+           "include",
+        }
+
+	filter { "system:windows", "not action:android" }
+
+            links {
+            "gdi32",
+            "psapi",
+              }
+
+        filter "system:linux"
+
+             links {
+                "X11",
+                "GL",
+                "pthread",
+               }
+
+	filter { "system:macosx", "not action:ios" }
+
+        	linkoptions {
+                        "-framework Cocoa",
+                        "-framework QuartzCore",
+                        "-framework OpenGL",
+                        "-weak_framework Metal",
+                    }
+
+	filter "action:android"
+
+             links {
+                "EGL",
+                "GLESv2",
+              }
+    	filter "action:ios"
+
+               linkoptions {
+                        "-framework CoreFoundation",
+                        "-framework Foundation",
+                        "-framework OpenGLES",
+                        "-framework UIKit",
+                        "-framework QuartzCore",
+                        "-weak_framework Metal",
+                }
 
         filter {}
 
-        includedirs 
-	{
-        }
-     
- 
     end)
+
+    zpm.uses {
+	"Zefiros-Software/bx"
+    }
+
