43a0bdfdc668cc17ce5601875d807b38a3fd8f80
PaulVisscher <paul.pev@hotmail.com>
2017-01-21 16:18:24 +0100

    updated build settings

--- test/zpm.lua
+++ test/zpm.lua
@@ -10,7 +10,11 @@ workspace "bgfx"
 
         zpm.uses "Zefiros-Software/bgfx"
 
-        includedirs "extern/Zefiros-Software/bgfx-@head/examples/common/"
+        includedirs {
+		    "extern/Zefiros-Software/bgfx-@head/3rdparty",
+            "extern/Zefiros-Software/bgfx-@head/examples/common/"
+        }
         files {
-            "extern/Zefiros-Software/bgfx-@head/examples/common/**.cpp"
+            "extern/Zefiros-Software/bgfx-@head/examples/common/**.cpp",
+            "extern/Zefiros-Software/bgfx-@head/examples/common/**.c"
         }
