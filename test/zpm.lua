75bca9b7d065a5f47b4f34a15b17071c6c7741b6
PaulVisscher <paul.pev@hotmail.com>
2017-01-21 16:26:12 +0100

    Link compress code

--- test/zpm.lua
+++ test/zpm.lua
@@ -17,4 +17,6 @@ workspace "bgfx"
         files {
             "extern/Zefiros-Software/bgfx-@head/examples/common/**.cpp",
             "extern/Zefiros-Software/bgfx-@head/examples/common/**.c"
+            "extern/Zefiros-Software/bgfx-@head/examples/3rdparty/ib-compress/**.cpp",
+            "extern/Zefiros-Software/bgfx-@head/examples/3rdparty/ib-compress/**.c"
         }
