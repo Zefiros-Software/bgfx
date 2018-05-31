4a56d7fcd9317b6bb5ba0f865e27f12cd549bc49
p.e.visscher <p.e.visscher@zefiros.eu>
2018-05-31 12:30:34 +0200

    UPdate test

--- test/main.cpp
+++ test/main.cpp
@@ -1,18 +1,11 @@
 /*
- * Copyright 2011-2017 Branimir Karadzic. All rights reserved.
- * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
- */
-
-/*
  * Copyright 2011-2018 Branimir Karadzic. All rights reserved.
  * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
  */
 
 #include <bx/uint32_t.h>
-#include "common.h"
-#include "bgfx_utils.h"
+#include "bgfx/embedded_shader.h"
 #include "logo.h"
-#include "imgui/imgui.h"
 
 namespace
 {
@@ -52,8 +45,6 @@ public:
 			, 1.0f
 			, 0
 			);
-
-		imguiCreate();
 	}
 
 	virtual int shutdown() override
@@ -70,20 +61,6 @@ public:
 	{
 		if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState) )
 		{
-			imguiBeginFrame(m_mouseState.m_mx
-				,  m_mouseState.m_my
-				, (m_mouseState.m_buttons[entry::MouseButton::Left  ] ? IMGUI_MBUT_LEFT   : 0)
-				| (m_mouseState.m_buttons[entry::MouseButton::Right ] ? IMGUI_MBUT_RIGHT  : 0)
-				| (m_mouseState.m_buttons[entry::MouseButton::Middle] ? IMGUI_MBUT_MIDDLE : 0)
-				,  m_mouseState.m_mz
-				, uint16_t(m_width)
-				, uint16_t(m_height)
-				);
-
-			showExampleDialog(this);
-
-			imguiEndFrame();
-
 			// Set view 0 default viewport.
 			bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height) );
 
